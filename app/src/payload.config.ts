8d048b9932a6b19bf1f66a860b11617f680c341d
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-02-12 16:37:03 +0530

    posts collection created with given fields

--- app/src/payload.config.ts
+++ app/src/payload.config.ts
@@ -8,6 +8,7 @@ import { buildConfig } from 'payload/config'
 import {lexicalEditor } from '@payloadcms/richtext-lexical'
 
 import Users from './collections/Users'
+import Posts from './collections/Posts'
 
 export default buildConfig({
   admin: {
@@ -15,7 +16,7 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: lexicalEditor(),
-  collections: [Users],
+  collections: [Users,Posts],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
